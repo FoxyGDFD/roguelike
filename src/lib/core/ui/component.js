cedbba2554cc7e285b1074f748c1a0da20b407f8
PanfilovMS <panfilovms@git.corp.miigaik.ru>
2025-09-13 18:16:28 +0300

    feat: component define events, attributes and properties

--- src/lib/core/ui/component.js
+++ src/lib/core/ui/component.js
@@ -1,14 +1,20 @@
 /** @typedef {import('@core/class').ClassConstructor} ClassConstructor */
 var Class = $import('@core/class');
-// var effect = $import('@core/signal').effect;
+var effect = $import('@core/signal').effect;
 
 var templateCache = {};
+
+var reactiveAttributesRegexp = /\[(\w+)\]/;
+var reactivePropertyRegexp = /\{\{([\w | ' ']+)\}\}/;
+var eventPropertyRegexp = /on-(\w+)/;
 
 /** @type {ClassConstructor} */
 var Component = Class({
   constructor: function (templateUrl) {
     this._templateUrl = templateUrl;
     this.element = null;
+    this.bindings = [];
+    this._context = this._createRootContext();
   },
 
   methods: {
@@ -29,40 +35,26 @@ var Component = Class({
       callback(templateCache[this._templateUrl]);
     },
 
-    _createEventBinding: function (node, eventAttributeName) {
-      var eventName = eventAttributeName.slice(3);
-      var handler = this[node.getAttribute(eventAttributeName)];
-
-      node.removeAttribute(eventAttributeName);
-
-      var self = this;
-      node.addEventListener(eventName, function (e) {
-        if (typeof handler === 'function') {
-          handler.call(self, e);
-        }
-      });
-
-      this.disposeEventHandlers.push(function () {
-        return node.EventListener(eventName);
-      });
+    _createRootContext: function () {
+      return {
+        data: this,
+        locals: {},
+        parent: null,
+        root: null,
+      };
     },
 
-    _processElement: function (node) {
+    _processElement: function (node, context) {
       var attributes = node.getAttributeNames();
 
-      attributes.forEach(
-        function (attr) {
-          if (eventPropertyRegexp.test(attr)) {
-            this._createEventBinding(node, attr);
-            // eslint-disable-next-line no-console
-            console.log('Element event:', attr);
-          }
-          if (reactiveAttributesRegexp.test(attr)) {
-            // eslint-disable-next-line no-console
-            console.log('Element attribute:', attr);
-          }
-        }.bind(this)
-      );
+      attributes.forEach(function (attr) {
+        if (eventPropertyRegexp.test(attr)) {
+          console.log('Element event:', attr);
+        }
+        if (reactiveAttributesRegexp.test(attr)) {
+          console.log('Element attribute:', attr);
+        }
+      });
       var childNodes = node.childNodes;
       for (var i = 0; i < childNodes.length; i++) {
         var childNode = childNodes[i];
@@ -70,37 +62,38 @@ var Component = Class({
           childNode.nodeType === Node.TEXT_NODE &&
           childNode.textContent.trim() !== ''
         ) {
-          this._processText(childNode);
+          this._processText(childNode, context);
         }
       }
     },
 
-    _processText: function (textNode) {
+    _processText: function (textNode, context) {
       var text = textNode.textContent;
       if (reactivePropertyRegexp.test(text)) {
-        // eslint-disable-next-line no-console
         console.log('Element property:', text);
       }
     },
 
-    _processNode: function (node) {
+    _processNode: function (node, context) {
       switch (node.nodeType) {
         case Node.ELEMENT_NODE:
-          return this._processElement(node);
+          return this._processElement(node, context);
         case Node.TEXT_NODE:
-          return this._processText(node);
+          return this._processText(node, context);
         default:
-          return;
+          return context;
       }
     },
 
     _createNodeTree: function (element) {
-      var stack = [element];
+      var stack = [{ node: element, context: this._context }];
 
       while (stack.length > 0) {
-        var node = stack.pop();
+        var templateNode = stack.pop();
+        var context = templateNode.context;
+        var node = templateNode.node;
 
-        this._processNode(node);
+        this._processNode(node, context);
 
         // console.log(node.children);
         // If node was removed
@@ -111,7 +104,10 @@ var Component = Class({
         // add children to stack
         if (node.children && node.children.length > 0) {
           for (var i = node.children.length - 1; i >= 0; i--) {
-            stack.push(node.children[i]);
+            stack.push({
+              node: node.children[i],
+              context: context,
+            });
           }
         }
       }
@@ -133,7 +129,7 @@ var Component = Class({
 
       this._loadTemplate(function (templateString) {
         self.element = self._parseTemplate(templateString);
-
+        self._compile(self.element);
         self.onInit();
       });
 
